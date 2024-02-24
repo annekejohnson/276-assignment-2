11392ec8f22cc68a38f266cf1e2e58e9d334e335
Anneke Lee Johnson <alj3@sfu.ca>
2024-02-23 16:36:18 -0800

    initializing

---
+++ src/test/java/a2276/assignment2/Assignment2ApplicationTests.java
@@ -0,0 +1,13 @@
+package a2276.assignment2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
